c3c1a34ea99e7cfd38119ccfcb00b51c138de0d3
miusarnameBotslovers <oscar.develop@botslovers.com>
2025-07-19 12:20:08 -0500

    feat: :sparkles: Se crea el modelo y la migracion para Task

---
+++ app/Models/Task.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+        'user_id',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'completed' => 'boolean',
+        'due_date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
