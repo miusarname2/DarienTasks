b7b3bdf4fe5eed8f0f3fb7f1777848c321225c35
miusarnameBotslovers <oscar.develop@botslovers.com>
2025-07-20 05:22:09 -0500

    feat: :sparkles: Se crearon las rutas de login, register y logout

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\api\Auth\LoginController;
+use App\Http\Controllers\api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login',    [LoginController::class,    'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [LoginController::class, 'logout']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
