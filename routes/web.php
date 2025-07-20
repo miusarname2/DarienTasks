ad3d594bbeffe1d7377dbcc0f13a03f1373b2f04
miusarnameBotslovers <oscar.develop@botslovers.com>
2025-07-20 07:05:34 -0500

    feat: :sparkles: Se configuran las rutas(frontend) para hacer el CRUD

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,23 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('tasks', function () {
+        return Inertia::render('tasks');
+    })->name('tasks');
+
+    Route::get('tasks/create', function () {
+        return Inertia::render('taskCreate');
+    })->name('taskCreate');
+
+    Route::get('tasks/edit', function () {
+        return Inertia::render('taskEdit');
+    })->name('taskEdit');
+
+    Route::get('tasks/detail', function () {
+        return Inertia::render('taskDetail');
+    })->name('taskDetail');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
